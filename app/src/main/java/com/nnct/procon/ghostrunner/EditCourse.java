0e4f4373145d64aeb90e70c5f66479b99a4120b3
kaito <charainaomae2@hotmail.co.jp>
2017-09-30 12:03:36 +0900

    mobile-α

---
+++ app/src/main/java/com/nnct/procon/ghostrunner/EditCourse.java
@@ -0,0 +1,16 @@
+package com.nnct.procon.ghostrunner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by kaito on 2017/09/30.
+ */
+
+public class EditCourse extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+    }
+}
